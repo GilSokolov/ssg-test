adb95349e66cc307519dadf8a32a60cc81269e8b
Gil Sokolov <gilsokolov.gs@gmail.com>
2026-03-23 16:20:58 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { ContactUs } from './pages/contact-us/contact-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'contact-us',
+    component: ContactUs,
+  },
+];
